34747814375a1d191f4d5dc282a0becc6ffcbbb7
Valeriy Pogorelov <valeriy.pogorelov@together.com>
2021-09-19 23:35:45 +0300

    Docker fixes

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {FC} from 'react'
+import { Layout as AntdLayout, Menu } from 'antd';
 
-function Layout() {
+const { Header, Content } = AntdLayout;
+
+
+const Layout: FC = () => {
     return (
-        <div>
-            
-        </div>
+        <AntdLayout>
+            <Header>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 50px' }}>
+                <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+            </Content>
+        </AntdLayout>
     )
 }
 
